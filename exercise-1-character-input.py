d61c7cfd1efa4c2a0ca55ff472bb76be25075e5b
NetmanJD <62785767+CypherkingJD@users.noreply.github.com>
2023-12-06 23:05:41 -0600

    Update exercise-1-character-input.py
    updated with extension 1

--- exercise-1-character-input.py
+++ exercise-1-character-input.py
@@ -1,6 +1,9 @@
 name = input("What is your name? ")
 age = int(input("What is your age? "))
+number_copies = int(input("How many copies? "))
 current_year = 2023
 year_born = 2023-age
 year_plus_100 = year_born+100
-print(f"You will be one hundred years old in {year_plus_100}!"
+copy = 0
+while copy <= number_copies:
+  print(f"You will be one hundred years old in {year_plus_100}!"
